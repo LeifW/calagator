5ff361c20a0fc3c62deb0efef555ed32013b6a31
LeifW <abimelech@gmail.com>
2009-02-09 15:54:16 -0800

    Towards integrating stand-alone test into Calagator structure.

--- app/models/source_parser/myspace_calendar.rb
+++ app/models/source_parser/myspace_calendar.rb
@@ -1,13 +1,29 @@
-require 'rubygems'
-require 'hpricot'
-require 'open-uri'
+class SourceParser
+  class MyspaceCal < Base
+    require 'rubygems'
+    require 'hpricot'
+    require 'open-uri'
 
-doc = Hpricot(open("http://www.myspace.com/valentineslifeblood"))
-doc.search("/html/body/table/tr/td[2]/table/tr/td[3]/table[4]/tr[2]/td/table/tr").each do |row|
+    # Returns a set of AbstractEvent objects.
+    #
+    # Options:
+    # * :url => URL String to read events from.
+    def self.to_abstract_events(opts = {})
+
+    doc = Hpricot(open("http://www.myspace.com/valentineslifeblood"))
+    doc.search("/html/body/table/tr/td[2]/table/tr/td[3]/table[4]/tr[2]/td/table/tr").map do |row|
 #  row.search("td[1]/table/tr/td[1]/font").inner_html
-  row.search("td[1]/table/tr").each do |datetime|
-    puts datetime.search("td[1]/font").inner_html
-    puts datetime.search("td[1]/font").inner_html
+      row.search("td[1]/table/tr").each do |datetime|
+        puts datetime.search("td[1]/font").inner_html
+        puts datetime.search("td[2]/font").inner_html
+        event             = AbstractEvent.new
+        event.start_time  = component.dtstart
+        event.title       = component.summary
+        event.description = component.description
+        event.end_time    = component.dtend
+        event.url         = component.url
+	return event
+      end
+    end
   end
 end
-
