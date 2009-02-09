8a59ec778837202436cd9db062e0de490d966833
LeifW <abimelech@gmail.com>
2009-02-09 05:24:21 -0800

    Just to be sure...

--- app/models/source_parser/myspace_calendar.rb
+++ app/models/source_parser/myspace_calendar.rb
@@ -1,2 +1,13 @@
-<xsl:apply-templates select="/html/body/table/tr/td[2]/table/tr/td[3]/table[4]/tr[2]/td/table"/>
+require 'rubygems'
+require 'hpricot'
+require 'open-uri'
+
+doc = Hpricot(open("http://www.myspace.com/valentineslifeblood"))
+doc.search("/html/body/table/tr/td[2]/table/tr/td[3]/table[4]/tr[2]/td/table/tr").each do |row|
+#  row.search("td[1]/table/tr/td[1]/font").inner_html
+  row.search("td[1]/table/tr").each do |datetime|
+    puts datetime.search("td[1]/font").inner_html
+    puts datetime.search("td[1]/font").inner_html
+  end
+end
 
