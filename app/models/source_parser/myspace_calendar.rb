2397c40b17a751ce70319cf5b9222032fa0d3546
Leif <abimelech@gmail.com>
2009-02-12 23:00:00 -0800

    Prettified variable name.

--- app/models/source_parser/myspace_calendar.rb
+++ app/models/source_parser/myspace_calendar.rb
@@ -9,9 +9,9 @@ class SourceParser
       doc.search("/html/body/table/tr/td[2]/table/tr/td[3]/table[4]/tr[2]/td/table/tr").map do |row|
         returning(AbstractEvent.new) do |event|
 #  row.search("td[1]/table/tr/td[1]/font").inner_html
-          dt = row.at("td[1]/table/tr")
-          date = dt.at("td[1]/font").inner_html
-          time = dt.at("td[2]/font").inner_html + "M" # Make it say "PM" at the end.
+          datetime = row.at("td[1]/table/tr")
+          date = datetime.at("td[1]/font").inner_html
+          time = datetime.at("td[2]/font").inner_html + "M" # Make it say "PM" at the end.
 	  title = row.at("td[2]/font/a").inner_html
           event.start_time  = Time.parse(date + " " + time)
           event.end_time    = event.start_time + (3 * 60 * 60)  # +3 hours (in seconds)
